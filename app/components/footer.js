db9ca26e6723103d1f55539d18db7b562d884adc
Ronit Shakya <shkronit@gmail.com>
2025-11-11 20:16:01 +0545

    footer update

---
+++ app/components/footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="bg-gray-900 text-white text-center p-4 mt-10">
+      <p>© 2025...All rights reserved.</p>
+    </footer>
+  )
+}
+
+export default Footer
