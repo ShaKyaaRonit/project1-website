3ff703642856b41296941e64735ed81ff83d6c64
Ronit Shakya <shkronit@gmail.com>
2025-11-12 16:50:15 +0545

    minor update

--- app/components/footer.js
+++ app/components/footer.js
@@ -1,7 +1,9 @@
 const Footer = () => {
   return (
-    <footer className="bg-gray-900 text-white text-center p-4 mt-10">
-      <p>© 2025...All rights reserved.</p>
+    <footer className="bg-gray-800 text-white text-center p-4 mt-10">
+    <p> Email: <span className="font-semibold">info@devloops.com</span></p>
+    <p> Address: Kathmandu, Nepal</p>
+    <p>© 2025...All rights reserved.</p> 
     </footer>
   )
 }
